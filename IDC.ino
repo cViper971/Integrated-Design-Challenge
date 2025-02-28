94facb89db07f0832ee35d4a4c4bc6e9954a0ff9
Vivek Chandy <vac23@duke.edu>
2025-02-28 10:43:37 -0500

    LCD

--- IDC.ino
+++ IDC.ino
@@ -1,12 +1,7 @@
 #include <Servo.h>
-#include <Wire.h> // I2C library, required for MLX90614
-#include <SparkFunMLX90614.h> //Search for this library within Arduino IDE. Tools --> Manage Libraries --> Search for SparkFun MLX90614 in the search bar
-
-//declare servos
-Servo servoLeft;
-Servo servoRight;
-
-IRTherm therm; // Create an IRTherm object to interact with throughout
+#include <Wire.h>
+#include <SparkFunMLX90614.h>
+#include <SoftwareSerial.h>
 
 //define QTI and LED pins
 #define leftQTI 49 
@@ -17,15 +12,27 @@ IRTherm therm; // Create an IRTherm object to interact with throughout
 #define greenpin 46
 #define bluepin 44
 
+#define TxPin 14
+
+//declare servos
+Servo servoLeft;
+Servo servoRight;
+
+IRTherm therm;
+
+SoftwareSerial LCD = SoftwareSerial(255, TxPin);
+
 //variable used to track hash count
 int hashCount;
 int score;
+int vals[5] = {-1,-1,-1,-1,-1};
 
 void setup() {
   //attach servos and set up LEDs
   servoLeft.attach(11);
   servoRight.attach(12);
 
+  //set onboard and external LED pins to output
   pinMode(redpin, OUTPUT);
   pinMode(greenpin, OUTPUT);
   pinMode(bluepin, OUTPUT);
@@ -36,27 +43,27 @@ void setup() {
   pinMode(A2, OUTPUT);
   setExternal(0,0,0);
 
+  //set up the thermal sensor
   Wire.begin();
   therm.begin();
   therm.setUnit(TEMP_F);
 
+
+  //define variables to keep track of score and hash counts
   hashCount = 0;
   score = 0;
 
-  Serial.begin(9600); //start the serial monitor so we can view the output
+  //start serial monitor and XBee
+  Serial.begin(9600);
   Serial2.begin(9600);
+  LCD.begin(9600);
+
+  rerender();
 }
 
 void loop() {
+  //get QTI readings
   int state = getState();
-  Serial.println(state);
-
-  if (Serial2.available()) {
-    char incoming = Serial2.read();
-    setExternal(0,255,255);
-    delay(250);
-    setExternal(0,0,0);
-  }
 
   //make decision based on state reading from QTIs
   switch (state) {
@@ -86,8 +93,6 @@ void loop() {
       handleHash();
       break;
   }
-
-  
 }
 
 void handleHash(){
@@ -97,28 +102,37 @@ void handleHash(){
   if(hashCount==1){
     setLED(255,0,0);
     senseObject();
+
+    forward();
+    while(getState()==7)
+      delay(1);
   }else if(hashCount==2){
     setLED(0,255,0);
     senseObject();
 
+    vals[3] = score;
     if (Serial2.available())
       Serial2.print(char(90+score));
+
     setExternal(0,0,255);
     delay(250);
     setExternal(0,0,0);
 
     servoLeft.detach();
     servoRight.detach();
+
+    while(true){
+      readVals();
+      delay(1000);
+    }
   }
+
   delay(500);
   setLED(0,0,0);
-  //go forward until off hash
-  forward();
-  while(getState()==7)
-    delay(1);
 }
 
 void senseObject(){
+  //update score if the object is found to be cold and update LED accordingly
   if (therm.read()&&therm.object()<63.5){
     score = hashCount;
     setExternal(0,255,0);
@@ -152,6 +166,25 @@ int getState(){
   return 4*qti1 + 2*qti2 + qti3;
 }
 
+void readVals(){
+  boolean changed = false;
+  while (Serial2.available()) {
+      char incoming = Serial2.read();
+
+      setExternal(0,255,255);
+      delay(200);
+      setExternal(0,0,0);
+      delay(200);
+
+      vals[incoming/10 - 6] = incoming%10;
+      
+      changed = true;
+    }
+
+    if(changed)
+      rerender();
+}
+
 
 //directional functions
 void turnRight(){
@@ -175,7 +208,7 @@ void stop(){
 }
 
 
-//function to set leds
+//functions to set LEDs
 void setLED(int r, int g, int b){
   analogWrite(redpin, 255-r);
   analogWrite(greenpin, 255-g);
@@ -186,4 +219,22 @@ void setExternal(int r, int g, int b){
   analogWrite(A0, 255-r);
   analogWrite(A1, 255-g);
   analogWrite(A2, 255-b);
+}
+
+void rerender(){
+  LCD.write(12); // clear
+  delay(10);
+  LCD.write(22); // no cursor no blink
+  delay(10);
+
+  LCD.print("|");
+  for(int n:vals){
+    delay(10);
+    if(n>-1)
+      LCD.print(n);
+    else
+      LCD.print(" ");
+    delay(10);
+    LCD.print("|");
+  }
 }
